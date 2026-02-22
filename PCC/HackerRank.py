4ffe1a15e20940d1a115a02fe089355529ed6799
Lucas Thai <thaianhquan2007@gmail.com>
2026-02-22 23:09:15 +1100

    Add new files

--- PCC/HackerRank.py
+++ PCC/HackerRank.py
@@ -1,10 +1,4 @@
-'''
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
-print("Here are the first three players on my team:")
-for player in players[:3]:
-    print(player, end=" ")
-'''
-
+#Dict question
 '''
 n = int(input())
 student_marks = {}
@@ -15,7 +9,27 @@ for _ in range(n):
 query_name = input()
 marks = student_marks[query_name]
 '''
+
+#Accessing values through dict key
+'''
 query = input('student name: ').lower().strip()
 student_marks = {'lucas': [30,40,50], 'emma': [80,90,90]}
 marks = student_marks[query]
-print(marks)
+'''
+
+# name, *line = input().split() 
+# name,*line = [1, 4, 6, 3, 4], Basically name = 1, line = [4, 6, 3, 4]
+
+#Exercise requires to set result to 2 decimal places
+''' print(f"{Values:.2f})'''
+# : -> start formatting
+# 2f -> 2 decimal places
+# Only use in f-string
+
+
+
+
+
+
+
+#Next
